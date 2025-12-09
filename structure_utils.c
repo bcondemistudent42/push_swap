e18ab521ed221a4c4456b041ca9db1bc7c0b620f
Balthazar Condemine <bcondemi@z4r4p7.42lyon.fr>
2025-12-09 15:11:29 +0100

    created header to declare properly the struct, fnished ft_push no leaks possible, modifed ft_swap to change var with -> for ptr directly, all normed except two comment for the futur, all passing cc with flags -Wall -Werror -Wextra

--- structure_utils.c
+++ structure_utils.c
@@ -6,72 +6,76 @@
 /*   By: bcondemi <bcondemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 21:45:31 by bcondemi          #+#    #+#             */
-/*   Updated: 2025/12/09 13:34:46 by bcondemi         ###   ########.fr       */
+/*   Updated: 2025/12/09 15:10:27 by bcondemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct s_stack 
-{
-    int	*tab; //[1, 2, 9, 4, 3]
-    int size; // 5
-}	t_stack;
+#include "header_struct.h"
 
-t_stack a;
-t_stack b;
-
-void	ft_swap(t_stack my_stack)
+void	ft_swap(t_stack *my_stack)
 {
 	int	temp;
 
-	if (my_stack.size == 0 || my_stack.size == 1)
+	if (my_stack->size == 0 || my_stack->size == 1)
 		return ;
-	temp = my_stack.tab[1];
-	my_stack.tab[1] = my_stack.tab[0];
-	my_stack.tab[0] = temp;
+	temp = my_stack->tab[1];
+	my_stack->tab[1] = my_stack->tab[0];
+	my_stack->tab[0] = temp;
 	return ;
 }
 
-void	ft_push(t_stack a, t_stack b)
+void	ft_push(t_stack *a, t_stack *b)
 {
 	int	i;
-	int new_tab[a.size + 1];
+	int	*new_tab;
 
-	if (b.size == 0)
-		return ;
-	i = 1;
-	new_tab[0] = b.tab[0];
-	while (i <= a.size)
+	if (b->size == 0)
 	{
-		new_tab[i] = a.tab[i - 1];
+		// to see if we need to free(b.tab);
+		return ;
+	}
+	new_tab = malloc((sizeof(int)) * (a->size + 1));
+	if (!new_tab)
+	{
+		// mayb free a.tab to see with other function
+		return ;
+	}
+	i = 1;
+	new_tab[0] = b->tab[0];
+	while (i - 1 < a->size)
+	{
+		new_tab[i] = a->tab[i - 1];
 		i++;
 	}
-	a.tab = new_tab;
-	a.size++;
+	free(a->tab);
+	a->tab = new_tab;
+	a->size++;
 	return ;
 }
 
-#include <stdio.h>
-int main()
-{
-	t_stack	test1;
-	t_stack test2;
-	int one[] = {1, 1};
-	int two[] = {5} ;
-	
-	test2.size = 1;
-	test1.size = 2;
-	test1.tab = one;
-	test2.tab = two;
-	
-	printf("my decimal == %d\n", test1.tab[0]);
-	printf("my decimal == %d\n", test1.tab[1]);
-	printf("my decimal == %d\n", test1.tab[2]);
-	// printf("my decimal == %d\n", test1.tab[4]);
-	printf("--------------------------\n");
-	ft_push(test1, test2);
-	// printf("my decimal == %d\nmy ptr == %d\n", test.tab[2], test.size);
-	printf("my decimal == %d\n", test1.tab[0]);
-	printf("my decimal == %d\n", test1.tab[1]);
-	printf("my decimal == %d\n", test1.tab[2]);
-	// printf("my decimal == %d\n", test1.tab[4]);
-}
+// #include <stdio.h>
+// int main()
+// {
+// 	t_stack	test1;
+// 	t_stack test2;
+// 	int *one = malloc(sizeof(int) * 2);
+// 	one[0] = 1;
+// 	one[1] = 2;
+// 	int *two = malloc(sizeof(int) * 1);
+// 	two[0] = 5;
+// 	test2.size = 1;
+// 	test1.size = 2;
+// 	test1.tab = one;
+// 	test2.tab = two;
+
+	// printf("my decimal == %d\n", test1.tab[0]);
+	// printf("my decimal == %d\n", test1.tab[1]);
+	// printf("my decimal == %d\n", test1.tab[2]);
+	// printf("--------------------------\n");
+// 	ft_push(&test1, &test2);;
+// 	printf("my decimal == %d\n", test1.tab[0]);
+// 	printf("my decimal == %d\n", test1.tab[1]);
+// 	printf("my decimal == %d\n", test1.tab[2]);
+// 	free(test1.tab);
+// 	free(test2.tab);
+// }
