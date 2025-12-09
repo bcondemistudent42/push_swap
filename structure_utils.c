3a4f61a48c0827f62a769e90807f5a2337661a92
Balthazar Condemine <bcondemi@z4r4p7.42lyon.fr>
2025-12-09 13:36:47 +0100

    ft_swap working, ft_push not working yet

--- structure_utils.c
+++ structure_utils.c
@@ -6,7 +6,7 @@
 /*   By: bcondemi <bcondemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 21:45:31 by bcondemi          #+#    #+#             */
-/*   Updated: 2025/12/09 12:57:44 by bcondemi         ###   ########.fr       */
+/*   Updated: 2025/12/09 13:34:46 by bcondemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,25 +31,47 @@ void	ft_swap(t_stack my_stack)
 	return ;
 }
 
-// void	ft_push(t_stack a, t_stack b)
-// {
-	
-// }
+void	ft_push(t_stack a, t_stack b)
+{
+	int	i;
+	int new_tab[a.size + 1];
+
+	if (b.size == 0)
+		return ;
+	i = 1;
+	new_tab[0] = b.tab[0];
+	while (i <= a.size)
+	{
+		new_tab[i] = a.tab[i - 1];
+		i++;
+	}
+	a.tab = new_tab;
+	a.size++;
+	return ;
+}
 
 #include <stdio.h>
 int main()
 {
-	t_stack	test;
-	int tab[] = {0, 1, 2, 3, 4} ;
-	test.size = 5;
-	test.tab = tab;
-	printf("my decimal == %d\n", test.tab[0]);
-	printf("my decimal == %d\n", test.tab[1]);
-	printf("my decimal == %d\n", test.tab[2]);
+	t_stack	test1;
+	t_stack test2;
+	int one[] = {1, 1};
+	int two[] = {5} ;
+	
+	test2.size = 1;
+	test1.size = 2;
+	test1.tab = one;
+	test2.tab = two;
+	
+	printf("my decimal == %d\n", test1.tab[0]);
+	printf("my decimal == %d\n", test1.tab[1]);
+	printf("my decimal == %d\n", test1.tab[2]);
+	// printf("my decimal == %d\n", test1.tab[4]);
 	printf("--------------------------\n");
-	ft_swap(test);
+	ft_push(test1, test2);
 	// printf("my decimal == %d\nmy ptr == %d\n", test.tab[2], test.size);
-	printf("my decimal == %d\n", test.tab[0]);
-	printf("my decimal == %d\n", test.tab[1]);
-	printf("my decimal == %d\n", test.tab[2]);
+	printf("my decimal == %d\n", test1.tab[0]);
+	printf("my decimal == %d\n", test1.tab[1]);
+	printf("my decimal == %d\n", test1.tab[2]);
+	// printf("my decimal == %d\n", test1.tab[4]);
 }
