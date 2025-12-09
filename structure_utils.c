538f017341f86754ef468fda6d58dbd141f0f29d
Balthazar Condemine <bcondemi@z1r2p5.42lyon.fr>
2025-12-09 22:10:33 +0100

    ft_rr finished and working

--- structure_utils.c
+++ structure_utils.c
@@ -6,7 +6,7 @@
 /*   By: bcondemi <bcondemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 21:45:31 by bcondemi          #+#    #+#             */
-/*   Updated: 2025/12/09 22:03:42 by bcondemi         ###   ########.fr       */
+/*   Updated: 2025/12/09 22:10:07 by bcondemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,13 @@ void	ft_rotate(t_stack *stack, char *str)
 	return ;
 }
 
+void	ft_rr(t_stack *stack1, t_stack *stack2)
+{
+	ft_rotate(stack1, "ra\n");
+	ft_rotate(stack2, "rb\n");
+	return ;
+}
+
 // int	main(int ac, char **av)
 // {
 // 	t_stack	a;
@@ -88,11 +95,11 @@ void	ft_rotate(t_stack *stack, char *str)
 // 	one[2] = 13;
 // 	one[3] = 18;
 // 	one[4] = 19;
-// 	int *two = malloc(sizeof(int) * 4);
+// 	int *two = malloc(sizeof(int) * 3);
 // 	two[0] = 55;
 // 	two[1] = 56;
 // 	two[2] = 57;
-// 	test2.size = 5;
+// 	test2.size = 3;
 // 	test1.size = 5;
 // 	test1.tab = one;
 // 	test2.tab = two;
@@ -101,19 +108,19 @@ void	ft_rotate(t_stack *stack, char *str)
 // 	printf("my decimal == %d\n", test1.tab[2]);
 // 	printf("my decimal == %d\n", test1.tab[1]);
 // 	printf("my decimal == %d\n", test1.tab[0]);
-// 	// printf("2 my decimal == %d\n", test2.tab[2]);
-	// printf("2 my decimal == %d\n", test2.tab[1]);
-	// printf("2 my decimal == %d\n", test2.tab[0]);
-	// printf("--------------------------\n");
-	// ft_rotate(&test1, "ra\n");
-	// printf("my decimal == %d\n", test1.tab[4]);
-	// printf("my decimal == %d\n", test1.tab[3]);
-	// printf("my decimal == %d\n", test1.tab[2]);
-	// printf("my decimal == %d\n", test1.tab[1]);
-	// printf("my decimal == %d\n", test1.tab[0]);
-	// printf("2 my decimal == %d\n", test2.tab[2]);
-	// printf("2 my decimal == %d\n", test2.tab[1]);
-	// printf("2 my decimal == %d\n", test2.tab[0]);
-	// free(test1.tab);
-	// free(test2.tab);
-}
+// 	printf("2 my decimal == %d\n", test2.tab[2]);
+// 	printf("2 my decimal == %d\n", test2.tab[1]);
+// 	printf("2 my decimal == %d\n", test2.tab[0]);
+// 	printf("--------------------------\n");
+// 	ft_rr(&test1, &test2);
+// 	printf("my decimal == %d\n", test1.tab[4]);
+// 	printf("my decimal == %d\n", test1.tab[3]);
+// 	printf("my decimal == %d\n", test1.tab[2]);
+// 	printf("my decimal == %d\n", test1.tab[1]);
+// 	printf("my decimal == %d\n", test1.tab[0]);
+// 	printf("2 my decimal == %d\n", test2.tab[2]);
+// 	printf("2 my decimal == %d\n", test2.tab[1]);
+// 	printf("2 my decimal == %d\n", test2.tab[0]);
+// 	free(test1.tab);
+// 	free(test2.tab);
+// }
